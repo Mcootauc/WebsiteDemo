fac2d5a87bd9250705776945d0f5121298f177e6
Nu-coder <131555685+Nu-coder@users.noreply.github.com>
2024-03-22 23:34:03 -0700

    Add files via upload
    I rewrote my code for the table using Vue and Flowbite now, so please have a look cause these things got me tweaking bad :(

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<script setup>
+</script>
+
+<template>
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <tr>
+        <th scope="col" class="px-6 py-3">
+          ID #
+        </th>
+        <th scope="col" class="px-6 py-3">
+          Work Name
+        </th>
+        <th scope="col" class="px-6 py-3">
+          Category
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          Walahi
+        </th>
+        <td class="px-6 py-4">
+          Silver
+        </td>
+        <td class="px-6 py-4">
+          Laptop
+        </td>
+        <td class="px-6 py-4">
+          <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View detail</a>
+        </td>
+      </tr>
+      <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          I'm
+        </th>
+        <td class="px-6 py-4">
+          White
+        </td>
+        <td class="px-6 py-4">
+          Laptop PC
+        </td>
+
+        <td class="px-6 py-4">
+          <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View detail</a>
+        </td>
+      </tr>
+      <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          sooooo
+        </th>
+        <td class="px-6 py-4">
+          Black
+        </td>
+        <td class="px-6 py-4">
+          Accessories
+        </td>
+
+        <td class="px-6 py-4">
+          <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View detail</a>
+        </td>
+      </tr>
+      <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          cooked
+        </th>
+        <td class="px-6 py-4">
+          Gray
+        </td>
+        <td class="px-6 py-4">
+          Phone
+        </td>
+        <td class="px-6 py-4">
+          <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View detail</a>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          man
+        </th>
+        <td class="px-6 py-4">
+          Red
+        </td>
+        <td class="px-6 py-4">
+          Wearables
+        </td>
+        <td class="px-6 py-4">
+          <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View detail</a>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
