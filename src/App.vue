daf3c1df0502eaa5b1096e49533759681f0fd234
Nu-coder <131555685+Nu-coder@users.noreply.github.com>
2024-03-25 14:52:28 -0700

    Add files via upload
    Update test data for the table

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,3 @@
-<script setup>
-</script>
-
 <template>
   <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -10,81 +7,23 @@
           ID #
         </th>
         <th scope="col" class="px-6 py-3">
-          Work Name
+          Category
         </th>
         <th scope="col" class="px-6 py-3">
-          Category
+          Status
         </th>
       </tr>
       </thead>
-      <tbody>
+      <tbody v-for="item in dataTable">
       <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-          Walahi
+          {{ item.id }}
         </th>
         <td class="px-6 py-4">
-          Silver
+          {{ item.workName }}
         </td>
         <td class="px-6 py-4">
-          Laptop
-        </td>
-        <td class="px-6 py-4">
-          <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View detail</a>
-        </td>
-      </tr>
-      <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-          I'm
-        </th>
-        <td class="px-6 py-4">
-          White
-        </td>
-        <td class="px-6 py-4">
-          Laptop PC
-        </td>
-
-        <td class="px-6 py-4">
-          <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View detail</a>
-        </td>
-      </tr>
-      <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-          sooooo
-        </th>
-        <td class="px-6 py-4">
-          Black
-        </td>
-        <td class="px-6 py-4">
-          Accessories
-        </td>
-
-        <td class="px-6 py-4">
-          <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View detail</a>
-        </td>
-      </tr>
-      <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-          cooked
-        </th>
-        <td class="px-6 py-4">
-          Gray
-        </td>
-        <td class="px-6 py-4">
-          Phone
-        </td>
-        <td class="px-6 py-4">
-          <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View detail</a>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-          man
-        </th>
-        <td class="px-6 py-4">
-          Red
-        </td>
-        <td class="px-6 py-4">
-          Wearables
+          {{ item.status }}
         </td>
         <td class="px-6 py-4">
           <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View detail</a>
@@ -95,6 +34,11 @@
   </div>
 
 </template>
+<script setup>
+const dataTable = [ {id: 1, workName: "plumetting", status: true},
+                    {id: 2, workName: "plumetting", status: false},
+                    {id: 3, workName: "plumetting", status: false}]
+</script>
 
 <style scoped>
 </style>
