fc2fa35a02f5507fb8cff31028c9f8c8746aa361
HungNT181 <nutheboi@gmail.com>
2024-04-07 18:24:25 -0700

    new website

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+// main.js file
+
+import VueDatepickerUi from 'vue-datepicker-ui'
+import 'vue-datepicker-ui/lib/vuedatepickerui.css';
+
+
+createApp(App).mount('#app')
