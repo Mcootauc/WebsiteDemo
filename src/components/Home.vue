8e8b9a78779299970f128e7ed0508fa0ed9757fe
Mitchell Cootauco <Mcootauc@lion.lmu.edu>
2024-04-15 00:54:36 -0700

    "added a little transition"

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -33,64 +33,70 @@
                     </td>
                 </tr>
                 <!-- Conditional rendering for additional details with new styles -->
-                <tr v-if="item.showDetails" class="border-b">
-                    <td colspan="4" class="px-6 py-4">
-                        <table
-                            class="w-full text-sm text-left text-gray-500 dark:text-gray-400"
-                        >
-                            <thead
-                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+                <transition name="slide-fade">
+                    <tr v-if="item.showDetails" class="border-b">
+                        <td colspan="4" class="px-6 py-4">
+                            <table
+                                class="w-full text-sm text-left text-gray-500 dark:text-gray-400"
                             >
-                                <tr>
+                                <thead
+                                    class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+                                >
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3">
+                                            Date Created
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Price
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Contact
+                                        </th>
+                                    </tr>
+                                    <tr class="border-b">
+                                        <td class="px-6 py-4">
+                                            {{ item.dateCreated }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ item.price }}
+                                        </td>
+                                        <td class="px-6 py-4">123-456-7890</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3">
+                                            Sub Category
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Location
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Labor Hours
+                                        </th>
+                                    </tr>
+                                    <tr class="border-b">
+                                        <td class="px-6 py-4">
+                                            {{ item.subCategory }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ item.location }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ item.laborHours }}
+                                        </td>
+                                    </tr>
                                     <th scope="col" class="px-6 py-3">
-                                        Date Created
+                                        Description
                                     </th>
-                                    <th scope="col" class="px-6 py-3">Price</th>
-                                    <th scope="col" class="px-6 py-3">
-                                        Contact
-                                    </th>
-                                </tr>
-                                <tr class="border-b">
-                                    <td class="px-6 py-4">
-                                        {{ item.dateCreated }}
-                                    </td>
-                                    <td class="px-6 py-4">{{ item.price }}</td>
-                                    <td class="px-6 py-4">123-456-7890</td>
-                                </tr>
-                                <tr>
-                                    <th scope="col" class="px-6 py-3">
-                                        Sub Category
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        Location
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        Labor Hours
-                                    </th>
-                                </tr>
-                                <tr class="border-b">
-                                    <td class="px-6 py-4">
-                                        {{ item.subCategory }}
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        {{ item.location }}
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        {{ item.laborHours }}
-                                    </td>
-                                </tr>
-                                <th scope="col" class="px-6 py-3">
-                                    Description
-                                </th>
-                                <tr>
-                                    <td class="px-6 py-4">
-                                        {{ item.description }}
-                                    </td>
-                                </tr>
-                            </thead>
-                        </table>
-                    </td>
-                </tr>
+                                    <tr>
+                                        <td class="px-6 py-4">
+                                            {{ item.description }}
+                                        </td>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </td>
+                    </tr>
+                </transition>
             </tbody>
         </table>
     </div>
@@ -125,5 +131,20 @@ function toggleDetails(index) {
 .detail-cell {
     padding-left: 2rem; /* Indent the content of detail cells */
     border-top: 1px solid #ccc; /* Add a top border for separation */
+}
+
+/* Active transition states */
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+    transition:
+        opacity 0.3s ease,
+        transform 0.3s ease;
+}
+
+/* Initial state for entering and final state for leaving */
+.slide-fade-enter,
+.slide-fade-leave-to {
+    opacity: 0;
+    transform: translateY(10px); /* Adjust the direction/magnitude as needed */
 }
 </style>
